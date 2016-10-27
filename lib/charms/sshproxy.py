20095fdb05060a3ac30bb3a44a358db631da4b2a
Adam Israel <adam.israel@canonical.com>
2016-10-27 16:02:18 -0400

    Remove debug logging

--- lib/charms/sshproxy.py
+++ lib/charms/sshproxy.py
@@ -1,6 +1,5 @@
 from charmhelpers.core.hookenv import (
     config,
-    log,
 )
 import paramiko
 from subprocess import (
@@ -14,33 +13,25 @@ def _run(cmd, env=None):
     """ Run a command, either on the local machine or remotely via SSH. """
     if isinstance(cmd, str):
         cmd = cmd.split() if ' ' in cmd else [cmd]
-    log("Running command: {}".format(cmd))
     cfg = config()
-    log("Checking configuration: %s" % cfg)
     if all(k in cfg for k in ['ssh-hostname', 'ssh-username', 'ssh-password']):
         host = cfg['ssh-hostname']
         user = cfg['ssh-username']
         passwd = cfg['ssh-password']
 
         if host and user and passwd:
-            log("Calling SSH")
             return ssh(cmd, host, user, passwd)
 
-    log("Calling local process")
     p = Popen(cmd,
               env=env,
               stdout=PIPE,
               stderr=PIPE)
-    log("Communicating with process")
     stdout, stderr = p.communicate()
-    log("Polling return code")
     retcode = p.poll()
     if retcode > 0:
-        log("Raising exception")
         raise CalledProcessError(returncode=retcode,
                                  cmd=cmd,
                                  output=stderr.decode("utf-8").strip())
-    log("Returning output/err")
     return (stdout.decode('utf-8').strip(), stderr.decode('utf-8').strip())
 
 
@@ -50,16 +41,13 @@ def ssh(cmd, host, user, password=None):
         temporary until this charm /is/ the VPE Router '''
 
     cmds = ' '.join(cmd)
-    log("Running command(s): " + cmds)
     client = paramiko.SSHClient()
     client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-    log("Connecting to %s" % host)
     client.connect(host, port=22, username=user, password=password)
 
     stdin, stdout, stderr = client.exec_command(cmds)
     retcode = stdout.channel.recv_exit_status()
     client.close()  # @TODO re-use connections
-    log("Checking output")
     if retcode > 0:
         output = stderr.read().strip()
         raise CalledProcessError(returncode=retcode, cmd=cmd,
--- reactive/sshproxy.py
+++ reactive/sshproxy.py
@@ -3,7 +3,6 @@ from charmhelpers.core.hookenv import (
     action_get,
     action_set,
     config,
-    log,
 )
 from charms.reactive import (
     remove_state,
@@ -27,19 +26,13 @@ def config_changed():
 def run_command():
     try:
         cmd = action_get('command')
-        log("Running command: " + cmd)
         output, err = charms.sshproxy._run(cmd)
-        log("Ran command: " + cmd)
         if len(err):
-            log("Action failed")
             action_fail("Command '{}' returned error code {}".format(cmd, err))
         else:
-            log("Action succeeded: " + output)
             action_set({'output': output})
     except subprocess.CalledProcessError as e:
-        log("Action failed.")
         action_fail('Command failed: %s (%s)' %
                     (' '.join(e.cmd), str(e.output)))
     finally:
         remove_state('actions.run')
-    log("Action done")
