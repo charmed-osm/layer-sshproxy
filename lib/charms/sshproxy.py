3365efdf8345b6c4d633065fb6b8484fad184769
Adam Israel <adam@adamisrael.com>
2017-08-04 17:57:24 -0400

    Fix security model
    Add functionality to automatically generate a ssh keypair on installation or demand, exposing only the public key for key-based authentication. This also moves to DEPRECATE the config option for ssh-private-key, which is inherently insecure, as it stores the private key in plaintext.
    
    Add copyright

--- lib/charms/sshproxy.py
+++ lib/charms/sshproxy.py
@@ -1,8 +1,27 @@
+"""Module to help with executing commands over SSH."""
+##
+# Copyright 2016 Canonical Ltd.
+# All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+##
+
 from charmhelpers.core.hookenv import (
     config,
 )
 import io
 import paramiko
+import os
 
 from subprocess import (
     Popen,
@@ -11,21 +30,10 @@ from subprocess import (
 )
 
 
-def _run(cmd, env=None):
-    """ Run a command, either on the local machine or remotely via SSH. """
+def run_local(cmd, env=None):
+    """Run a command locally."""
     if isinstance(cmd, str):
         cmd = cmd.split(' ') if ' ' in cmd else [cmd]
-
-    cfg = config()
-    if all(k in cfg for k in ['ssh-hostname', 'ssh-username',
-                              'ssh-password', 'ssh-private-key']):
-        host = cfg['ssh-hostname']
-        user = cfg['ssh-username']
-        passwd = cfg['ssh-password']
-        key = cfg['ssh-private-key']
-
-        if host and user and (passwd or key):
-            return ssh(cmd, host, user, passwd, key)
 
     p = Popen(cmd,
               env=env,
@@ -41,16 +49,41 @@ def _run(cmd, env=None):
     return (stdout.decode('utf-8').strip(), stderr.decode('utf-8').strip())
 
 
-def get_ssh_client(host, user, password=None, key=None):
-    """Return a connected Paramiko ssh object"""
+def _run(cmd, env=None):
+    """Run a command, either on the local machine or remotely via SSH."""
+    if isinstance(cmd, str):
+        cmd = cmd.split(' ') if ' ' in cmd else [cmd]
+    cfg = config()
 
+    if all(k in cfg for k in ['ssh-hostname', 'ssh-username',
+                              'ssh-password', 'ssh-private-key']):
+        host = cfg['ssh-hostname']
+        user = cfg['ssh-username']
+        passwd = cfg['ssh-password']
+        key = cfg['ssh-private-key']  # DEPRECATED
+
+        if host and user:
+            return ssh(cmd, host, user, passwd, key)
+
+    return run_local(cmd, env)
+
+
+def get_ssh_client(host, user, password=None, key=None):
+    """Return a connected Paramiko ssh object."""
     client = paramiko.SSHClient()
     client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 
     pkey = None
+
+    # Check for the DEPRECATED private-key
     if key:
         f = io.StringIO(key)
         pkey = paramiko.RSAKey.from_private_key(f)
+    else:
+        # Otherwise, check for the auto-generated private key
+        if os.path.exists('/root/.ssh/id_juju_sshproxy'):
+            with open('/root/.ssh/id_juju_sshproxy', 'r') as f:
+                pkey = paramiko.RSAKey.from_private_key(f)
 
     ###########################################################################
     # There is a bug in some versions of OpenSSH 4.3 (CentOS/RHEL 5) where    #
@@ -72,15 +105,15 @@ def get_ssh_client(host, user, password=None, key=None):
             # Once more, with feeling
             client.connect(host, port=22, username=user,
                            password=password, pkey=pkey)
-            pass
         else:
-            raise paramiko.ssh_exception.SSHException(e)
+            # Reraise the original exception
+            raise e
 
     return client
 
 
 def sftp(local_file, remote_file, host, user, password=None, key=None):
-    """Copy a local file to a remote host"""
+    """Copy a local file to a remote host."""
     client = get_ssh_client(host, user, password, key)
 
     # Create an sftp connection from the underlying transport
@@ -90,7 +123,7 @@ def sftp(local_file, remote_file, host, user, password=None, key=None):
 
 
 def ssh(cmd, host, user, password=None, key=None):
-    """ Run an arbitrary command over SSH. """
+    """Run an arbitrary command over SSH."""
     client = get_ssh_client(host, user, password, key)
 
     cmds = ' '.join(cmd)
